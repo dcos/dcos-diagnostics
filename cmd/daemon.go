5bc7f6fbfdcba981dfb3f28e2005c09155cfeb62
Tomasz Janiszewski <janiszt@gmail.com>
2018-08-23 14:16:26 +0200

    Fix command description
    The description of `daemon` was copied from cobra example
    and has noting to do with this project.
    Long description for help has invalid command `check` which is not
    implemented.

--- cmd/daemon.go
+++ cmd/daemon.go
@@ -46,13 +46,7 @@ var defaultStateURL = url.URL{
 // daemonCmd represents the daemon command
 var daemonCmd = &cobra.Command{
 	Use:   "daemon",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Start an http server and polls the components health.",
 	Run: func(cmd *cobra.Command, args []string) {
 		startDiagnosticsDaemon()
 	},
