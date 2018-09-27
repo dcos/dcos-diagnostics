7f862258c571b7cfd8bc37fd633368343c459d36
Tomasz Janiszewski <janiszt@gmail.com>
2018-09-27 17:55:22 +0200

    Replace custom config loading function with Viper
    Viper has a special function
    [`Unmarshal`](https://godoc.org/github.com/spf13/viper#Unmarshal)
    created specially for situation when we want to store configuration in
    custom structure. We hade similar function created by hand but it used
    json serialization as a bridge between `map[sting]interface{}` and
    `Config`.
    
    This change also removes dependency to `pkg/errors`.

---
+++ cmd/root_test.go
@@ -0,0 +1,52 @@
+// Copyright © 2017 Mesosphere Inc. <http://mesosphere.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"github.com/dcos/dcos-diagnostics/config"
+	"github.com/stretchr/testify/assert"
+	"path/filepath"
+	"testing"
+)
+
+func Test_initConfig(t *testing.T) {
+	cfgFile =  filepath.Join("testdata", "dcos-diagnostics-config.json")
+	defer func() { cfgFile = ""}()
+
+	initConfig()
+
+	expected := &config.Config{
+		FlagRole: "master",
+		FlagPort:1050,
+		FlagPull: true,
+		FlagMasterPort:1050,
+		FlagAgentPort:61001,
+		FlagPullInterval:60,
+		FlagPullTimeoutSec:3,
+		FlagUpdateHealthReportInterval:60,
+		FlagExhibitorClusterStatusURL:"http://127.0.0.1:8181/exhibitor/v1/cluster/status",
+		FlagDisableUnixSocket: true,
+		FlagDiagnosticsBundleDir:"diag-bundles",
+		FlagDiagnosticsBundleEndpointsConfigFile:"dcos-diagnostics-endpoint-config.json",
+		FlagDiagnosticsBundleUnitsLogsSinceString:"24h",
+		FlagDiagnosticsJobTimeoutMinutes:720,
+		FlagDiagnosticsJobGetSingleURLTimeoutMinutes:2,
+		FlagCommandExecTimeoutSec:120,
+	}
+
+
+	assert.Equal(t, expected, defaultConfig)
+
+}
--- config/config.go
+++ config/config.go
@@ -1,11 +1,5 @@
 package config
 
-import (
-	"encoding/json"
-
-	"github.com/pkg/errors"
-)
-
 var (
 	// Version of dcos-diagnostics code.
 	Version = "0.4.0"
@@ -16,44 +10,30 @@ var (
 
 // Config structure is a main config object
 type Config struct {
-	SystemdUnits []string `json:"-"`
+	SystemdUnits []string
 
 	// dcos-diagnostics flags
-	FlagCACertFile                 string `json:"ca-cert"`
-	FlagPull                       bool   `json:"pull"`
-	FlagVerbose                    bool   `json:"verbose"`
-	FlagPort                       int    `json:"port"`
-	FlagDisableUnixSocket          bool   `json:"no-unix-socket"`
-	FlagMasterPort                 int    `json:"master-port"`
-	FlagAgentPort                  int    `json:"agent-port"`
-	FlagPullInterval               int    `json:"pull-interval"`
-	FlagPullTimeoutSec             int    `json:"pull-timeout"`
-	FlagUpdateHealthReportInterval int    `json:"health-update-interval"`
-	FlagExhibitorClusterStatusURL  string `json:"exhibitor-ip"`
-	FlagForceTLS                   bool   `json:"force-tls"`
-	FlagDebug                      bool   `json:"debug"`
-	FlagRole                       string `json:"role"`
-	FlagIAMConfig                  string `json:"iam-config"`
+	FlagCACertFile                 string `mapstructure:"ca-cert"`
+	FlagPull                       bool   `mapstructure:"pull"`
+	FlagVerbose                    bool   `mapstructure:"verbose"`
+	FlagPort                       int    `mapstructure:"port"`
+	FlagDisableUnixSocket          bool   `mapstructure:"no-unix-socket"`
+	FlagMasterPort                 int    `mapstructure:"master-port"`
+	FlagAgentPort                  int    `mapstructure:"agent-port"`
+	FlagPullInterval               int    `mapstructure:"pull-interval"`
+	FlagPullTimeoutSec             int    `mapstructure:"pull-timeout"`
+	FlagUpdateHealthReportInterval int    `mapstructure:"health-update-interval"`
+	FlagExhibitorClusterStatusURL  string `mapstructure:"exhibitor-ip"`
+	FlagForceTLS                   bool   `mapstructure:"force-tls"`
+	FlagDebug                      bool   `mapstructure:"debug"`
+	FlagRole                       string `mapstructure:"role"`
+	FlagIAMConfig                  string `mapstructure:"iam-config"`
 
 	// diagnostics job flags
-	FlagDiagnosticsBundleDir                     string `json:"diagnostics-bundle-dir"`
-	FlagDiagnosticsBundleEndpointsConfigFile     string `json:"endpoint-config"`
-	FlagDiagnosticsBundleUnitsLogsSinceString    string `json:"diagnostics-units-since"`
-	FlagDiagnosticsJobTimeoutMinutes             int    `json:"diagnostics-job-timeout"`
-	FlagDiagnosticsJobGetSingleURLTimeoutMinutes int    `json:"diagnostics-url-timeout"`
-	FlagCommandExecTimeoutSec                    int    `json:"command-exec-timeout"`
-}
-
-// LoadFromViper takes a map of flags with values and updates the config structure.
-func (c *Config) LoadFromViper(settings map[string]interface{}) error {
-	// TODO(mnaboka): use a map to struct library
-	body, err := json.Marshal(settings)
-	if err != nil {
-		return errors.Wrap(err, "unable to marshal config file")
-	}
-
-	if err := json.Unmarshal(body, c); err != nil {
-		return errors.Wrap(err, "unable to unmarshal config file")
-	}
-	return nil
+	FlagDiagnosticsBundleDir                     string `mapstructure:"diagnostics-bundle-dir"`
+	FlagDiagnosticsBundleEndpointsConfigFile     string `mapstructure:"endpoint-config"`
+	FlagDiagnosticsBundleUnitsLogsSinceString    string `mapstructure:"diagnostics-units-since"`
+	FlagDiagnosticsJobTimeoutMinutes             int    `mapstructure:"diagnostics-job-timeout"`
+	FlagDiagnosticsJobGetSingleURLTimeoutMinutes int    `mapstructure:"diagnostics-url-timeout"`
+	FlagCommandExecTimeoutSec                    int    `mapstructure:"command-exec-timeout"`
 }
