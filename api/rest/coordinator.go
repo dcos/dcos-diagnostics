b5f70d473bc00b613729d940dfba98f7d1395182
Brian Lewis <keller@csh.rit.edu>
2019-06-18 17:15:46 +0200

    Pull coordinator out to separate PR and make necessary changes for it to compile

---
+++ api/rest/coordinator.go
@@ -0,0 +1,227 @@
+package rest
+
+import (
+	"archive/zip"
+	"context"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"time"
+
+	"github.com/sirupsen/logrus"
+)
+
+const numberOfWorkers = 10
+
+type BundleStatus struct {
+	ID   string
+	node node
+	done bool
+	err  error
+}
+
+// Coordinator is an interface to coordinate the creation of diagnostics bundles
+// across a cluster of nodes
+type Coordinator interface {
+	Create(ctx context.Context, id string, nodes []node) <-chan BundleStatus
+	Collect(ctx context.Context, bundleID string, numBundles int, statuses <-chan BundleStatus) (string, error)
+}
+
+// ParallelCoordinator implements Coordinator interface to coordinate bundle
+// creation across a cluster, parallelized across multiple goroutines
+type ParallelCoordinator struct {
+	client Client
+}
+
+// job is a function that will be called by worker. The output will be added to results chanel
+type job func() BundleStatus
+
+// worker is a function that will run incoming jobs from jobs channel and put jobs output to results chan
+func worker(_ context.Context, jobs <-chan job, results chan<- BundleStatus) {
+	for j := range jobs {
+		//TODO(janisz): Handle context
+		results <- j()
+	}
+}
+
+// NewParallelCoordinator constructs a new parallelcoordinator.
+func NewParallelCoordinator(client Client) ParallelCoordinator {
+	return ParallelCoordinator{
+		client: client,
+	}
+}
+
+// Create starts bundle creation process. Creation process could be monitor on returned channel.
+func (c ParallelCoordinator) Create(ctx context.Context, id string, nodes []node) <-chan BundleStatus {
+
+	jobs := make(chan job)
+	statuses := make(chan BundleStatus)
+
+	for i := 0; i < numberOfWorkers; i++ {
+		go worker(ctx, jobs, statuses)
+	}
+
+	for _, n := range nodes {
+		logrus.WithField("IP", n.IP).Info("Sending creation request to node.")
+
+		// necessary to prevent the closure from giving the same node to all the calls
+		tmpNode := n
+		jobs <- func() BundleStatus {
+			//TODO(janisz): Handle context
+
+			// because this will also make a request from the coordinating master, we
+			// can't tell it to make a local bundle with the same ID this guarantees that
+			// the coordinating master's local bundle has a different ID.
+			// from this point, the full <ip>-<id> will be carried in the BundleStatus ID field so this does not
+			// need to be recalculated in the coordinator
+			fullID := fmt.Sprintf("%s-%s", tmpNode.IP, id)
+			return c.createBundle(ctx, tmpNode, fullID, jobs)
+		}
+	}
+
+	return statuses
+}
+
+func (c ParallelCoordinator) Collect(ctx context.Context, bundleID string, numBundles int, statuses <-chan BundleStatus) (string, error) {
+
+	var bundles []string
+
+	for len(bundles) < numBundles {
+		select {
+		case <-ctx.Done():
+			return "", fmt.Errorf("context canceled before all node bundles finished")
+		case s := <-statuses:
+			if !s.done {
+				logrus.WithError(s.err).WithField("IP", s.node.IP).WithField("ID", s.ID).Info("Got status update. Bundle not ready.")
+				continue
+			}
+			if s.err != nil {
+				logrus.WithError(s.err).WithField("IP", s.node.IP).WithField("ID", s.ID).Warn("Bundle errored")
+				// TODO: the bundle will never finish, need to stop this from waiting forever
+				continue
+			}
+
+			//TODO(janisz): Handle file creation in workdir
+			// the '*' will be swapped out with a random string by ioutil.TempFile
+			// This will use the default temp directory from os.TempDir
+			destinationFile, err := ioutil.TempFile("", fmt.Sprintf("bundle-%s-*.zip", bundleID))
+			if err != nil {
+				return "", fmt.Errorf("could not create temporary result file")
+			}
+			_ = destinationFile.Close()
+
+			err = c.client.GetFile(ctx, s.node.baseURL, s.ID, destinationFile.Name())
+			if err != nil {
+				logrus.WithError(err).WithField("IP", s.node.IP).WithField("ID", s.ID).Warn("Could not download file")
+			}
+
+			bundles = append(bundles, destinationFile.Name())
+		}
+	}
+
+	return mergeZips(bundleID, bundles)
+}
+
+func mergeZips(bundleID string, bundlePaths []string) (string, error) {
+
+	bundleName := fmt.Sprintf("bundle-%s-*.zip", bundleID)
+	mergedZip, err := ioutil.TempFile("", bundleName)
+	if err != nil {
+		return "", err
+	}
+	defer mergedZip.Close()
+
+	zipWriter := zip.NewWriter(mergedZip)
+	defer zipWriter.Close()
+
+	for _, p := range bundlePaths {
+		err = appendToZip(zipWriter, p)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	return mergedZip.Name(), nil
+}
+
+func appendToZip(writer *zip.Writer, path string) error {
+	r, err := zip.OpenReader(path)
+	if err != nil {
+		return fmt.Errorf("could not open %s: %s", path, err)
+	}
+	defer r.Close()
+
+	for _, f := range r.File {
+		rc, err := f.Open()
+		if err != nil {
+			return fmt.Errorf("could not open %s from zip: %s", f.Name, err)
+		}
+
+		file, err := writer.Create(f.Name)
+		if err != nil {
+			return fmt.Errorf("could not create file %s: %s", f.Name, err)
+		}
+		_, err = io.Copy(file, rc)
+		if err != nil {
+			return fmt.Errorf("could not copy file %s to zip: %s", f.Name, err)
+		}
+		rc.Close()
+	}
+
+	return nil
+}
+
+func (c ParallelCoordinator) createBundle(ctx context.Context, node node, id string, jobs chan<- job) BundleStatus {
+	_, err := c.client.CreateBundle(ctx, node.baseURL, id)
+	if err != nil {
+		// Return done status with error. To mark node as errored so file will not be downloaded
+		return BundleStatus{
+			ID:   id,
+			node: node,
+			done: true,
+			err:  fmt.Errorf("could not create bundle: %s", err),
+		}
+	}
+
+	// Schedule bundle status check
+	jobs <- func() BundleStatus {
+		return c.waitForDone(ctx, node, id, jobs)
+	}
+
+	// Return undone status with no error.
+	return BundleStatus{ID: id, node: node}
+}
+
+func (c ParallelCoordinator) waitForDone(ctx context.Context, node node, id string, jobs chan<- job) BundleStatus {
+	statusCheck := func() {
+		jobs <- func() BundleStatus {
+			return c.waitForDone(ctx, node, id, jobs)
+		}
+	}
+
+	//TODO(janisz): Handle context
+
+	logrus.WithField("IP", node.IP).Info("Checking bundle status on node.")
+	// Check bundle status
+	bundle, err := c.client.Status(ctx, node.baseURL, id)
+	// If error
+	if err != nil {
+		// then schedule next check in given time.
+		// It will only add check to job queue so interval might increase but it's OK.
+		time.AfterFunc(time.Second, statusCheck)
+		// Return status with error. Do not mark bundle as done yet. It might change it status
+		return BundleStatus{ID: id, node: node, err: fmt.Errorf("could not check status: %s", err)}
+	}
+	// If bundle is in terminal state (its state won't change)
+	if bundle.Status == Done || bundle.Status == Deleted || bundle.Status == Canceled {
+		logrus.WithField("IP", node.IP).Info("Node bundle is finished.")
+		// mark it as done
+		return BundleStatus{ID: id, node: node, done: true}
+	}
+	// If bundle is still in progress (InProgress, Unknown or Started)
+	// then schedule next check in given time
+	// It will only add check to job queue so interval might increase but it's OK.
+	time.AfterFunc(time.Second, statusCheck)
+	// Return undone status with no error. Do not mark bundle as done yet. It might change it status
+	return BundleStatus{ID: id, node: node}
+}
---
+++ api/rest/coordinator_test.go
@@ -0,0 +1,143 @@
+package rest
+
+import (
+	"archive/zip"
+	"context"
+	"fmt"
+	"net"
+	"os"
+	"path/filepath"
+	"runtime"
+	"sort"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCoordinator_CreatorShouldCreateAbundleAndReturnUpdateChan(t *testing.T) {
+
+	numGoroutine := runtime.NumGoroutine()
+
+	client := new(MockClient)
+
+	c := ParallelCoordinator{
+		client: client,
+	}
+
+	ctx := context.TODO()
+
+	node1 := node{IP: net.ParseIP("192.0.2.1"), baseURL: "http://192.0.2.1"}
+	node2 := node{IP: net.ParseIP("192.0.2.2"), baseURL: "http://192.0.2.2"}
+	node3 := node{IP: net.ParseIP("192.0.2.3"), baseURL: "http://192.0.2.3"}
+	testNodes := []node{
+		node1,
+		node2,
+		node3,
+	}
+
+	expected := []BundleStatus{}
+
+	for _, n := range testNodes {
+		id := fmt.Sprintf("%s-%s", n.IP, "id")
+		client.On("CreateBundle", ctx, n.baseURL, id).Return(&Bundle{ID: id, Status: Started}, nil)
+		client.On("Status", ctx, n.baseURL, id).Return(&Bundle{ID: id, Status: Done}, nil)
+
+		expected = append(expected,
+			BundleStatus{ID: id, node: n},
+			BundleStatus{ID: id, node: n, done: true},
+		)
+	}
+	s := c.Create(context.TODO(), "id", testNodes)
+
+	var statuses []BundleStatus
+
+	assert.Equal(t, numberOfWorkers, runtime.NumGoroutine()-numGoroutine)
+
+	for i := 0; i < 6; i++ {
+		statuses = append(statuses, <-s)
+	}
+
+	for _, s := range statuses {
+		assert.Contains(t, expected, s)
+	}
+}
+
+func TestCoordinatorCreateAndCollect(t *testing.T) {
+	//TODO(janisz): FIXME
+	t.Skipf("Uncoment this test after we figure out how to generate temp local bundle dir")
+	client := MockClient{}
+
+	c := ParallelCoordinator{
+		client: &client,
+	}
+
+	ctx := context.TODO()
+
+	bundleID := "bundle-0"
+	numNodes := 3
+
+	node1 := node{IP: net.ParseIP("192.0.2.1"), baseURL: "http://192.0.2.1"}
+	node2 := node{IP: net.ParseIP("192.0.2.2"), baseURL: "http://192.0.2.2"}
+	node3 := node{IP: net.ParseIP("192.0.2.3"), baseURL: "http://192.0.2.3"}
+
+	testZip1, err := filepath.Abs(filepath.Join("testdata", "192.0.2.1.zip"))
+	require.NoError(t, err)
+	testZip2, err := filepath.Abs(filepath.Join("testdata", "192.0.2.2.zip"))
+	require.NoError(t, err)
+	testZip3, err := filepath.Abs(filepath.Join("testdata", "192.0.2.3.zip"))
+	require.NoError(t, err)
+
+	testNodes := []struct {
+		n       node
+		zipPath string
+	}{
+		{
+			n:       node1,
+			zipPath: testZip1,
+		},
+		{
+			n:       node2,
+			zipPath: testZip2,
+		},
+		{
+			n:       node3,
+			zipPath: testZip3,
+		},
+	}
+
+	for _, testData := range testNodes {
+		id := fmt.Sprintf("%s-%s", testData.n.IP, bundleID)
+		client.On("CreateBundle", ctx, testData.n.baseURL, id).Return(&Bundle{ID: id, Status: Started}, nil)
+		client.On("Status", ctx, testData.n.baseURL, id).Return(&Bundle{ID: id, Status: Done}, nil)
+		client.On("GetFile", ctx, testData.n.baseURL, id, testData.zipPath).Return(nil)
+	}
+
+	statuses := c.Create(ctx, "bundle-0", []node{node1, node2, node3})
+
+	bundlePath, err := c.Collect(ctx, bundleID, numNodes, statuses)
+	require.NoError(t, err)
+	defer os.RemoveAll(bundlePath)
+	require.NotEmpty(t, bundlePath)
+
+	zipReader, err := zip.OpenReader(bundlePath)
+	require.NoError(t, err)
+	defer zipReader.Close()
+
+	expectedContents := []string{
+		"192.0.2.1/",
+		"192.0.2.1/test.txt",
+		"192.0.2.2/",
+		"192.0.2.2/test.txt",
+		"192.0.2.3/",
+		"192.0.2.3/test.txt",
+	}
+
+	filenames := []string{}
+	for _, f := range zipReader.File {
+		filenames = append(filenames, f.Name)
+	}
+	sort.Strings(filenames)
+
+	assert.Equal(t, expectedContents, filenames)
+}
