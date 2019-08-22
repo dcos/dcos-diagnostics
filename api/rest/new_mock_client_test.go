b34ba55e81177da8eed20a0629d03ded5643bf6a
Tomasz Janiszewski <janiszt@gmail.com>
2019-08-22 14:07:16 +0200

    Make ID case consistent

--- api/rest/new_mock_client_test.go
+++ api/rest/new_mock_client_test.go
@@ -7,15 +7,15 @@ type MockClient struct {
 	status       func(ctx context.Context, node string, ID string) (*Bundle, error)
 	getFile      func(ctx context.Context, node string, ID string, path string) (err error)
 	list         func(ctx context.Context, node string) ([]*Bundle, error)
-	delete       func(ctx context.Context, node string, id string) error
+	delete       func(ctx context.Context, node string, ID string) error
 }
 
 func (_m *MockClient) CreateBundle(ctx context.Context, node string, ID string) (*Bundle, error) {
 	return _m.createBundle(ctx, node, ID)
 }
 
-func (_m *MockClient) Delete(ctx context.Context, node string, id string) error {
-	return _m.delete(ctx, node, id)
+func (_m *MockClient) Delete(ctx context.Context, node string, ID string) error {
+	return _m.delete(ctx, node, ID)
 }
 
 func (_m *MockClient) GetFile(ctx context.Context, node string, ID string, path string) error {
