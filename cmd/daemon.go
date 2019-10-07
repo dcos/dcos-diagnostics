ca4440bf5f7b4d7ed83b39633132b35884f95a5a
Tomasz Janiszewski <janiszt@gmail.com>
2019-10-07 15:48:33 +0200

    Revert "Change default status check interval to 5 seconds from a minute (#151)"
    This reverts commit 1c0970b9c685f33fec7e966b9e486d5797ebf7f2.
    
    Creating local bundle takes much more than 5s. We don't need query node
    every 5s about its status.
    
    This change will reduce logs noise on larger cluster.

--- cmd/daemon.go
+++ cmd/daemon.go
@@ -109,7 +109,7 @@ func startDiagnosticsDaemon() {
 		logrus.WithError(err).Fatal("BundleHandler could not be created")
 	}
 	diagClient := rest.NewDiagnosticsClient(client)
-	coord := rest.NewParallelCoordinator(diagClient, 5*time.Second, defaultConfig.FlagDiagnosticsBundleDir)
+	coord := rest.NewParallelCoordinator(diagClient, time.Minute, defaultConfig.FlagDiagnosticsBundleDir)
 	urlBuilder := diagDcos.NewURLBuilder(defaultConfig.FlagAgentPort, defaultConfig.FlagMasterPort, defaultConfig.FlagForceTLS)
 	clusterBundleHandler, err := rest.NewClusterBundleHandler(coord, diagClient, DCOSTools, defaultConfig.FlagDiagnosticsBundleDir,
 		bundleTimeout, &urlBuilder)
