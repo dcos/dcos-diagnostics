2dc70de91eaa91c054448d5598975efa420144ea
janisz <tjaniszewski@d2iq.com>
2020-09-28 16:03:04 +0200

    Update bundle_handler_test.go

--- api/rest/bundle_handler_test.go
+++ api/rest/bundle_handler_test.go
@@ -826,7 +826,7 @@ func TestIfE2E_(t *testing.T) {
 		MockCollector{name: "collector-4", rc: slowReader{delay: time.Millisecond}},
 	}
 
-	bh, err := NewBundleHandler(workdir, collectors, time.Second, time.Second)
+	bh, err := NewBundleHandler(workdir, collectors, time.Second, 100 * time.Millisecond)
 	require.NoError(t, err)
 	bh.clock = &MockClock{now: now}
 
